446f92c17988285333b220049ea76aa38708cbdc
David.Rz.Liu <mania7539@gmail.com>
2017-12-01 16:58:08 +0800

    controlled components and binding context/this

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,14 +1,43 @@
 import React, { Component } from "react";
 
 export default class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            term: ""
+        };
+
+        this.onInputChange = this.onInputChange.bind(this);
+        // the first 'this' on the right side which is 'SearchBar'
+        // has a function called 'onInputChange'
+        // bind the function to 'this' which is 'SearchBar'
+        // to replace the function 'onInputChange' of 'this' which is 'SearchBar'
+    }
+
+    onInputChange(event) {
+        console.log(event.target.value);
+        this.setState({ term: event.target.value });
+
+    }
+
     render() {
         return (
             <form className="input-group">
-                <input className="form-control" />
+                <input
+                    placeholder="Get a five-day forecast in your favorite cities"
+                    className="form-control"
+                    value={this.state.term}
+                    onChange={this.onInputChange}
+                />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
             </form>
         );
     }
+    // 'onChange={(event) => this.onInputChange(event)}' 
+    //      is equal to
+    // 'onChange={this.onInputChange}' + 'this.onInputChange = this.onInputChange.bind(this)'
+    // 		so use an arrow function would be easier in this case
 }
