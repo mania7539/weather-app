73c243edf8dcbe50fb98db7161ee22e75d5d4644
David.Rz.Liu <mania7539@gmail.com>
2017-12-01 18:52:08 +0800

    design an ajax request with axios in action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,20 @@
+import axios from "axios";
 
 const API_KEY = "8f88977c65d644e0270a474780baf026";
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?apikey=${API_KEY}`;
+// use ES6 formatted string syntax
+
+export const FETCH_WEATHER = "FETCH_WEATHER";
+// make the name and the value independent for encapsulation so   
+// we can just use the const and don't have to worry about its value
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`;
+    // use 'us' for country code as default country
+    const request = axios.get(url);
+
+    return ({
+        type: FETCH_WEATHER,
+        payload: request
+    });
+}
